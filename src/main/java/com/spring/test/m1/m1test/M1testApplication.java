256f3b310690641ceab4ff5cfe000597d6342c05
Teoh Kheng Hong <kh.teoh@maybank.com>
2023-04-03 14:32:52 +0800

    init commit

---
+++ src/main/java/com/spring/test/m1/m1test/M1testApplication.java
@@ -0,0 +1,13 @@
+package com.spring.test.m1.m1test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/test/m1/m1test/M1testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.test.m1.m1test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
